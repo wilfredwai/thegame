cd24806e635b30158384ee0873d5cc1e9f143191
wilfredwai <wilfredwilfred@gmail.com>
2026-01-14 14:44:35 +0800

    add circleshape.py

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock() #clock
+    dt = 0
     loop = 1
     while loop > 0:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
+        
     fill(black, rect=None, special_flags=0) # type: ignore
     pygame.display.flip()
 
