2e0800ee366af481ec7e68dd6873614d9acc5af9
wilfredwai <wilfredwilfred@gmail.com>
2026-01-15 09:25:15 +0800

    add player.update

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ def main():
         screen.fill("black") # type: ignore
         player.draw(screen)
         pygame.display.flip()
+        player.update(dt)
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import PLAYER_RADIUS, LINE_WIDTH
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
 from circleshape import CircleShape
 
 class Player(CircleShape):
@@ -9,6 +9,18 @@ class Player(CircleShape):
 
     def draw(self, screen):
         pygame.draw.polygon(screen, (255, 255, 255), self.triangle(), LINE_WIDTH)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt)
+
+        if keys[pygame.K_d]:
+            self.rotate(-dt)
 
     # in the Player class
     def triangle(self):
